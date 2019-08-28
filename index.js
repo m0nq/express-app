7866d5e7470d9ebbf4cad920cb872d3038d2ea73
Monk Wellington <forgetmenaught@gmail.com>
2019-08-28 12:08:07 -0700

    Added data to server

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 import express from 'express';
+import data from './data/data';
 
 const app = express();
 const PORT = 3000;
 
 app.get('/', (req, res) => res.send('Hello World!'));
 
-app.listen(PORT, () => console.log(`Listening on the quiet storm... *${PORT}`));
+app.listen(PORT, () => {
+  console.log(`Listening on the quiet storm... *${PORT}`);
+  console.log('data ->', data);
+});
