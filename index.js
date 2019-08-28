000eeffcdfa020f9aab473089e4eee5464d4ac4e
Monk Wellington <forgetmenaught@gmail.com>
2019-08-28 14:01:57 -0700

    Return json data from get endpoint

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express();
 const PORT = 3000;
 
 app.get('/', (req, res) => {
-  res.send(`A get request with / route on port ${PORT}`);
+  res.json(data);
 });
 
 app.post('/newItem', (req, res) => {
